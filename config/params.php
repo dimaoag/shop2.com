9075dd2dc078f2b834ddcc0182c5239ada6dc7ae
Dmytro Orikhovskyi <dimaoag@gmail.com>
2018-04-20 23:56:40 +0300

    add Singletone, params, registry

---
+++ config/params.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'admin_email' => 'testingoda@gmail.com',
+    'shop_name' => 'shop2.com',
+    'pagination' => 3,
+    'smtp_login' => '',
+    'smtp_password' => '',
+];
--- public/index.php
+++ public/index.php
@@ -1,3 +1,7 @@
 <?php
 
 require_once dirname(__DIR__) . "/config/init.php";
+require_once LIBS . "/functions.php";
+new \shop2\App();
+
+debug(\shop2\App::$app->getProperties());
